0f711e626e0003108b021bb063a9a47f63d0bfce
Sophia Franco de Godoy <161141869+sophiagodoy@users.noreply.github.com>
2024-09-06 15:28:38 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,43 @@
+// ARQUIVO PARA DADOS 
+// CRIANDO VARIÁVEIS - LISTAS E OBJETOS 
+let dados = [
+    {
+        titulo: "Base da pirâmide",
+        descricao: `Cereais, pães e tubérculos:
+        - São a principal fonte de energia para o nosso corpo.
+        - Fornecem carboidratos complexos que são liberados lentamente, garantindo energia duradoura.
+        - Opte pelas versões integrais, que são mais ricas em fibras e nutrientes.`,
+        link: "https://pt.wikipedia.org/wiki/Pir%C3%A2mide_alimentar"
+    },
+    {
+        titulo: "Meio da piramide",
+        descricao: `Hortaliças e Frutas:
+        - Ricas em vitaminas, minerais, fibras e antioxidantes.
+        - Fortalecem o sistema imunológico.
+        - Ajudam na digestão.
+        - Protegem o corpo contra doenças.
+        - Varie as cores para garantir a ingestão de diferentes nutrientes.
+        
+        Leguminosas:
+        - Feijão, lentilha, grão de bico e outras leguminosas são excelentes fontes de proteínas vegetais, fibras e minerais como ferro.
+        - Ajudam a controlar o colesterol e a saciedade.
+        
+        Carnes, aves, peixes e ovos:
+        - Fornecem proteínas de alta qualidade, essenciais para o crescimento e reparo dos tecidos.
+        - Priorize as carnes magras e os peixes, que são mais saudáveis.
+        
+        Leite e derivados:
+        - Ricos em cálcio, fósforo e proteínas, são importantes para a saúde dos ossos e dentes.
+        - Opte por versões desnatadas ou semidesnatadas.`,
+        link: "https://pt.wikipedia.org/wiki/Pir%C3%A2mide_alimentar"
+    },
+    {
+        titulo: "Topo de pirâmide",
+        descricao: `Óleos e Gorduras:
+        - Devem ser consumidos com moderação e preferencialmente os de origem vegetal, como azeite de oliva e óleo de canola.
+        
+        Açúcares e Doces:
+        - Devem ser consumidos ocasionalmente, em pequenas quantidades, pois o excesso pode levar ao ganho de peso e outras doenças.`,
+        link: "https://pt.wikipedia.org/wiki/Pir%C3%A2mide_alimentar" 
+    }
+];
